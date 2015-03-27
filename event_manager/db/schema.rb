0cbcb14bf97ca30661158762d08fbb061a83dbc3
Will <williamrwilson@gmail.com>
2015-03-27 11:52:32 +0000

    add events, need to add foreign keys

--- event_manager/db/schema.rb
+++ event_manager/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326222139) do
+ActiveRecord::Schema.define(version: 20150327003729) do
+
+  create_table "events", force: true do |t|
+    t.string   "location"
+    t.datetime "date"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
